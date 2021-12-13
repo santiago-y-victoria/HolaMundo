53515cfbbf315b763b018298217b72132b543e44
vjrincon <vjrincon@misena.edu.co>
2021-12-13 18:54:02 -0500

    Update C++ Potencia.cpp
    he identado el codigo

--- C++ Potencia.cpp
+++ C++ Potencia.cpp
@@ -1,26 +1,23 @@
 #include <conio.h>
 #include <iostream>
 using namespace std;
+main()
+	 {
 
- main()
- {
- 
-int  b,a;
-int i;
-int potencia=1;
+		int  b,a;
+		int i;
+		int potencia=1;
 
+		cout<<"Ingresar la exponente:  ";
+		cin>>b;
+		cout << endl;
+		cout<<"Ingresar el base:  ";
+		cin>>a;
+		cout << endl;
 
-cout<<"Ingresar la exponente:  ";
-cin>>b;
-cout << endl;
-cout<<"Ingresar el base:  ";
-cin>>a;
-cout << endl;
-
-for (i=1; i <= b; i++)
-{
-	potencia = potencia * a;
-}
-
-cout<<potencia;
-}
+		for (i=1; i <= b; i++)
+			{
+				potencia = potencia * a;
+			}
+		cout<<potencia;
+	}
