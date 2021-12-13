b6b7ced33def88a496946b81dba6e4f242684606
vjrincon <vjrincon@misena.edu.co>
2021-12-13 12:49:43 -0500

    Update C++ Potencia.cpp
    solo tabulación identación

--- C++ Potencia.cpp
+++ C++ Potencia.cpp
@@ -3,24 +3,20 @@
 using namespace std;
 
  main()
- {
- 
-int  b,a;
-int i;
-int potencia=1;
+	 {
+		int  b,a;
+		int i;
+		int potencia=1;
 
-
-cout<<"Ingresar la exponente:  ";
-cin>>b;
-cout << endl;
-cout<<"Ingresar el base:  ";
-cin>>a;
-cout << endl;
-
-for (i=1; i <= b; i++)
-{
-	potencia = potencia * a;
-}
-
-cout<<potencia;
-}
+		cout<<"Ingresar la exponente:  ";
+		cin>>b;
+		cout << endl;
+		cout<<"Ingresar el base:  ";
+		cin>>a;
+		cout << endl;
+		for (i=1; i <= b; i++)
+			{
+				potencia = potencia * a;
+			}
+		cout<<potencia;
+	}
