d6750b8de3f0c154f6ee2f5306c9ebe4c9809e44
SandraBulla <skbulla4@misena.edu.co>
2021-12-12 21:58:01 -0500

    Codigo

---
+++ C++ Potencia.cpp
@@ -0,0 +1,26 @@
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+ main()
+ {
+ 
+int  b,a;
+int i;
+int potencia=1;
+
+
+cout<<"Ingresar la exponente:  ";
+cin>>b;
+cout << endl;
+cout<<"Ingresar el base:  ";
+cin>>a;
+cout << endl;
+
+for (i=1; i <= b; i++)
+{
+	potencia = potencia * a;
+}
+
+cout<<potencia;
+}
